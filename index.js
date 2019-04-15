62b53b6b43af7d880d67c58ac8aade10203baa31
outbounder <obiwonn@gmail.com>
2019-04-15 21:52:47 +0300

    add support for angel compose run

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = async function (angel) {
   require('angelabilities-exec')(angel)
   require('./tasks/compose')(angel)
   require('./tasks/develop')(angel)
+  require('./tasks/run')(angel)
 }
--- tasks/develop.js
+++ tasks/develop.js
@@ -1,11 +1,17 @@
 const path = require('path')
+const findSkeletonRoot = require('organic-stem-skeleton-find-root')
 
 module.exports = function (angel) {
-  angel.on('compose develop', function () {
+  angel.on('compose develop', async function () {
+    let repoRoot = await findSkeletonRoot()
+    let projectName = require(path.join(repoRoot, 'package.json')).name
     let cellName = require(path.join(process.cwd(), 'package.json')).name
     process.on('SIGINT', function () {
-      angel.exec(`npx angel compose | docker-compose -p devshell -f - down ${cellName}`)
+      angel.exec(`npx angel compose | docker-compose -p ${projectName} -f - down ${cellName}`)
     })
-    angel.exec(`npx angel compose | docker-compose -p devshell -f - up ${cellName}`)
+    process.on('SIGTERM', function () {
+      angel.exec(`npx angel compose | docker-compose -p ${projectName} -f - down ${cellName}`)
+    })
+    angel.exec(`npx angel compose | docker-compose -p ${projectName} -f - up ${cellName}`)
   })
 }
