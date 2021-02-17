dfaabebcbb4f9d231ca101866c4b1f29bda8d332
outbounder <obiwonn@gmail.com>
2021-02-17 14:47:01 +0200

    rework support for default settings per cell

--- tasks/compose-yaml.js
+++ tasks/compose-yaml.js
@@ -5,53 +5,37 @@ const findSkeletonRoot = require('organic-stem-skeleton-find-root')
 
 module.exports = function (angel) {
   angel.on(/compose.yaml -- (.*)/, async function (angel) {
-    let REPO = await findSkeletonRoot()
+    const REPO = await findSkeletonRoot()
     const loadRootDNA = require(path.join(REPO, 'cells/node_modules/lib/load-root-dna'))
     const packagejson = require(process.cwd() + '/package.json')
-    let DNA = await loadRootDNA()
-    let cells = getCells(DNA.cells)
-    let cellPorts = DNA['cell-ports']
-    let cellMountpoints = DNA['cell-mountpoints']
+    const DNA = await loadRootDNA()
+    const cells = getCells(DNA.cells)
+    const cellPorts = DNA['cell-ports']
+    const cellMountpoints = DNA['cell-mountpoints']
+    const commonDeps = packagejson['common_dependencies']
 
     let composeJSON = {
       version: '3.4',
       services: {}
     }
     for (let i = 0; i < cells.length; i++) {
-      if (cells[i].name !== packagejson.name) continue
       let cell = cells[i]
-      if (cell.dna.dockerCompose) {
-        composeJSON.services[cell.name] = cell.dna.dockerCompose
-        continue
-      }
-
-      // without CWD the cellCompose bellow doesnt make any sense
+      if (cell.name !== packagejson.name) continue
       if (!cell.dna.cwd) continue
+
       let nodeVersion = '11.10.1'
       if (packagejson.engines && packagejson.engines.node) {
         nodeVersion = packagejson.engines.node
       }
-
       let cellCompose = {
         image: `node:${nodeVersion}-alpine`,
         labels: {},
-        volumes: [
-          `${REPO}:/workdir`,
-        ],
-        working_dir: `/workdir/${cell.dna.cwd}`,
+        volumes: [],
+        working_dir: `/${cell.dna.cwd}`,
         command: angel.cmdData[1],
         ports: [],
         environment: {
           USER: process.env.USER
-        }
-      }
-      if (cell.dna.compose) {
-        let composeConfig = cell.dna.compose
-        if (composeConfig.volumes) {
-          cellCompose.volumes = cellCompose.volumes.concat(composeConfig.volumes)
-        }
-        if (composeConfig.ports) {
-          cellCompose.ports = cellCompose.ports.concat(composeConfig.ports)
         }
       }
       if (cellMountpoints[cell.name]) {
@@ -63,7 +47,19 @@ module.exports = function (angel) {
         let cellPort = cellPorts[cell.name]
         cellCompose.ports = cellCompose.ports.concat([`${cellPort}:${cellPort}`])
       }
-      composeJSON.services[cell.name] = cellCompose
+
+      cellCompose.volumes.push(`${REPO}/cells:/cells`)
+      cellCompose.volumes.push(`${REPO}/dna:/dna`)
+      if (commonDeps) {
+        commonDeps.forEach(dep => {
+          let absolute_path = `${REPO}/cells/node_modules/${dep}`
+          let depRelativePathFromCell = path.relative(process.cwd(), absolute_path)
+          let depRepoPath = path.relative(REPO, absolute_path)
+          cellCompose.volumes.push(`${depRelativePathFromCell}:/${depRepoPath}`)
+        })
+      }
+
+      composeJSON.services[cell.name] = Object.assign(cellCompose, cell.dna.dockerCompose)
     }
     console.log(YAML.stringify(composeJSON))
   })
